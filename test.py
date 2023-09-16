6953b99e4993082f0dc0b602ec589ccac9bc10df
Nikolay St. Dimitrov <126271396+nikolay-st-d@users.noreply.github.com>
2023-09-16 14:22:04 +0300

    Some changes

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
-male = 1
-female = 1
-family = male + female
-some_condom = 'Any brand'
-
-
 def use_condom(condom):
     if condom:
         return True
     return False
 
 
+male = 1
+female = 1
+family = male + female
+some_condom = 'Any brand'
+
 if not use_condom(some_condom):
     family = 3
     print('Game Over!')
+else:
+    print('Live your life and be happy!')
